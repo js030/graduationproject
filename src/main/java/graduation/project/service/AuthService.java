cc985bd480f09eea26bea5cb43947c08395c3fef
kyumho <pray96@naver.com>
2022-03-29 00:25:25 +0900

    my second commit

--- src/main/java/graduation/project/service/AuthService.java
+++ src/main/java/graduation/project/service/AuthService.java
@@ -3,6 +3,7 @@ package graduation.project.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Transactional
 @RequiredArgsConstructor
